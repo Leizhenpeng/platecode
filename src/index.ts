b6c9174d2a6bae7ad225d8e72e7f3510cf7b637d
river <laolei@forkway.cn>
2024-07-06 09:11:01 +0800

    chore: Update npm dependencies for crypto-js to version 4.2.0

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,44 @@
-export const one = 1
-export const two = 2
+import { Buffer } from 'node:buffer'
+
+export class CryptoPlate {
+  private province: string
+  private alphabet: string
+
+  constructor() {
+    this.province = '黑吉辽京津晋冀鲁豫蒙沪渝苏浙皖闽湘赣鄂桂琼川贵云藏陕甘宁青新粤'
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  }
+
+  public encode(input: string): string {
+    const encoded = Buffer.from(input).toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '') // URL安全的Base64，删除填充
+    return this.generateLicensePlates(encoded)
+  }
+
+  public decode(licensePlates: string): string {
+    const encoded = this.extractEncodedText(licensePlates).replace(/-/g, '+').replace(/_/g, '/')
+    const paddedEncoded = encoded.padEnd(encoded.length + (4 - encoded.length % 4) % 4, '=') // 重新添加必要的填充
+    return Buffer.from(paddedEncoded, 'base64').toString('utf8')
+  }
+
+  private generateLicensePlates(encoded: string): string {
+    const chunkSize = 5
+    let chunks = encoded.match(new RegExp(`.{1,${chunkSize}}`, 'g')) as string[] || []
+    // 不转换为大写
+    chunks = chunks.map(chunk => chunk.padEnd(chunkSize, 'A'))
+
+    return chunks.map((chunk) => {
+      const province = this.province[Math.floor(Math.random() * this.province.length)]
+      const letter = this.alphabet[Math.floor(Math.random() * this.alphabet.length)]
+      return `${province}${letter}·${chunk}`
+    }).join(' ')
+  }
+
+  private extractEncodedText(licensePlates: string): string {
+    return licensePlates.split(' ').map((plate) => {
+      // 移除额外的填充字符'A'
+      return plate.split('·')[1].replace(/A+$/, '')
+    }).join('')
+  }
+}
+
+export default CryptoPlate
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,25 @@
+/* eslint-disable no-console */
 import { describe, expect, it } from 'vitest'
+import { CryptoPlate } from '../src'
 
-describe('should', () => {
-  it('exported', () => {
-    expect(1).toEqual(1)
+describe('cryptoPlate', () => {
+  const crypto = new CryptoPlate()
+
+  it('should encode a string to a single string of multiple license plates', () => {
+    const originalString = 'Hello, World! This is a test string to be encoded.'
+    const licensePlates = crypto.encode(originalString)
+    console.log('Encoded License Plates:', licensePlates)
+
+    const platesArray = licensePlates.split(' ')
+    platesArray.forEach((plate) => {
+      expect(plate).toMatch(/^[\u4E00-\u9FA5][A-Z]·[A-Za-z0-9+/=]{5}$/)
+    })
+  })
+
+  it('should decode the license plates string back to the original string', () => {
+    const originalString = 'Hello, World! This is a test string to be encoded.'
+    const licensePlates = crypto.encode(originalString)
+    const decodedString = crypto.decode(licensePlates)
+    expect(decodedString).toBe(originalString)
   })
 })
